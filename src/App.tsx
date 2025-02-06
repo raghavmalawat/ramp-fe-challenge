a0e92b50868638c780318286350e67aa83225b7b
Raghav Malawat <30802908+raghavmalawat@users.noreply.github.com>
2025-02-06 01:44:55 +0000

    bugfix:5:set isLoading to false after employee fetch is done

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,8 @@ export function App() {
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
-    await paginatedTransactionsUtils.fetchAll()
-
     setIsLoading(false)
+    await paginatedTransactionsUtils.fetchAll()
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(
